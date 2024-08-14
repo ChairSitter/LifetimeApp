627f72479f557796c0fe895cc4e805e73affcf47
Yogi Spice <caseynnewman@yahoo.com>
2024-08-14 14:27:53 -0400

    adjusted file path

--- server/server.ts
+++ server/server.ts
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelizeRename.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
       console.log(`App listening on port ${PORT}!`)
     })
