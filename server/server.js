1777a4d495e6487696ff12123ad2b1e599484ada
Yogi Spice <caseynnewman@yahoo.com>
2024-08-14 13:50:16 -0400

    added server and connection files

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// link to other file / dotenv
+
+const PORT = process.env.PORT || 3001
+const app = express();
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+
+app.use(session(sess));
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`)
+    })
+  });
