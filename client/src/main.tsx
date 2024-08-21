93a7af99a6efbdd9ac5f9f4234fc733505a8da74
Yogi Spice <caseynnewman@yahoo.com>
2024-08-21 14:13:30 -0400

    created pages for each view and added react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.tsx';
+import './index.css';
+
+import Daily from './pages/DayView/index.tsx';
+import Calendar from './pages/CalendarView/index.tsx';
+import Lifetime from './pages/LifetimeView/index.tsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <React.StrictMode>
+      <ChakraProvider disableGlobalStyle={true} resetCSS={false}>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>,
+    // errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Calendar />,
+      },
+      {
+        path: '/daily',
+        element: <Daily />,
+      },
+      {
+        path: '/lifetime',
+        element: <Lifetime />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
